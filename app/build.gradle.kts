9e99a846937bdeb2137933462b75c6fec4c6c26d
yangsooplus <yangsooplus@naver.com>
2025-12-29 22:22:43 +0900

    release: 1.1.4 version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
 
     defaultConfig {
         applicationId = "com.oksusu.susu"
-        versionCode = 12
-        versionName = "1.1.3"
+        versionCode = 15
+        versionName = "1.1.4"
         buildConfigField("String", "KAKAO_APP_KEY", getApiKey("KAKAO_APP_KEY"))
         manifestPlaceholders["KAKAO_APP_KEY"] = "kakao${getApiKey("KAKAO_REDIRECT_KEY")}"
     }
